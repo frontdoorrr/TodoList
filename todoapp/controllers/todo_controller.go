00217921c7f1666bc98a2917a1cdc34ef54c8047
JEONGMUN LEE <lot9616@gmail.com>
2024-07-31 23:07:43 +0900

    update

--- todoapp/controllers/todo_controller.go
+++ todoapp/controllers/todo_controller.go
@@ -8,12 +8,29 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// GetTodos godoc
+// @Summary Get all todos
+// @Description Get all todos
+// @Tags todos
+// @Accept  json
+// @Produce  json
+// @Success 200 {array} models.Todo
+// @Router /todos [get]
 func GetTodos(c *gin.Context) {
     var todos []models.Todo
     database.DB.Find(&todos)
     c.JSON(http.StatusOK, todos)
 }
 
+// CreateTodo godoc
+// @Summary Create a todo
+// @Description Create a new todo
+// @Tags todos
+// @Accept  json
+// @Produce  json
+// @Param todo body models.Todo true "Todo"
+// @Success 201 {object} models.Todo
+// @Router /todos [post]
 func CreateTodo(c *gin.Context) {
     var todo models.Todo
     if err := c.BindJSON(&todo); err != nil {
