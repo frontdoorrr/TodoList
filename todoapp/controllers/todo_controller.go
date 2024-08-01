e365b9fdc2251ac3d5e9d455569286f8ef2fa876
JEONGMUN LEE <lot9616@gmail.com>
2024-08-01 21:09:59 +0900

    Update todo_controller.go

--- todoapp/controllers/todo_controller.go
+++ todoapp/controllers/todo_controller.go
@@ -8,29 +8,33 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// GetTodos godoc
-// @Summary Get all todos
-// @Description Get all todos
-// @Tags todos
-// @Accept  json
-// @Produce  json
-// @Success 200 {array} models.Todo
-// @Router /todos [get]
+
 func GetTodos(c *gin.Context) {
     var todos []models.Todo
     database.DB.Find(&todos)
     c.JSON(http.StatusOK, todos)
 }
 
-// CreateTodo godoc
-// @Summary Create a todo
-// @Description Create a new todo
-// @Tags todos
-// @Accept  json
-// @Produce  json
-// @Param todo body models.Todo true "Todo"
-// @Success 201 {object} models.Todo
-// @Router /todos [post]
+func GetTodoByPK(c *gin.Context) {
+	var todo models.Todo
+	pk := c.Param("pk")
+	if err := database.DB.First(&todo, pk).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error" : "Todo Not Found"})
+	}
+
+	database.DB.First(&todo, pk)
+	c.JSON(http.StatusOK, todo)
+}
+
+func Welcome(c *gin.Context){
+	firstname := c.DefaultQuery("firstname", "Guest")
+	lastname := c.Query("lastname")
+
+	c.String(http.StatusOK, "Hello %s %s", firstname, lastname)
+}
+
+
+
 func CreateTodo(c *gin.Context) {
     var todo models.Todo
     if err := c.BindJSON(&todo); err != nil {
